8e7980842566caf61103f776d0de4f28704be883
suportQC-NC <rhprodev@gmail.com>
2026-03-08 17:04:36 +1100

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+       <h1>Welcome to the MVP Frontend!</h1>
+       <p>This is a placeholder for the main application content.</p>
+    </>
+  )
+}
+
+export default App
